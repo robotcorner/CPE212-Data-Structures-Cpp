e6b2a069a49814123e2ebaf665a58ed14e0f899d
Stephen M Stammen <sms0066@uah.edu>
2019-11-06 13:06:14 -0600

    updated files to allow more than one project

--- lab8_stephen_stammen_jakob_henson/graph_driver.cpp
+++ lab8_stephen_stammen_jakob_henson/graph_driver.cpp
@@ -27,6 +27,7 @@ int main()
   output.open("out1.txt");
   
   cout << "things work here" << endl;
+  cout << "both gdb and valgrind don't determine the problem" << endl;
   GraphType<int> intGraph(MAX_V); // set max input for vertices as 100 for testing
   cout << "things don't work here" << endl;
   
@@ -34,30 +35,30 @@ int main()
   int vFrom, vTo, i = 0;
 
   cout << "Starting the Graph by List Program" << endl;
-  /*
+
   // Test 1
   input >> vFrom >> vTo;
   cout << "vfrom: " << vFrom << " vTo: " << "\n";
-  intGraph.AddEdge(vFrom, vTo, arcLength);
+  //intGraph.AddEdge(vFrom, vTo, arcLength);
 
   // Test 2
   input >> vFrom >> vTo;
   cout << "vfrom: " << vFrom << " vTo: " << "\n";
-  intGraph.AddEdge(vFrom, vTo, arcLength);
+  //intGraph.AddEdge(vFrom, vTo, arcLength);
 
   // Test 3
   input >> vFrom >> vTo;
   cout << "vfrom: " << vFrom << " vTo: " << "\n";
-  intGraph.AddEdge(vFrom, vTo, arcLength);
-  */
+  //intGraph.AddEdge(vFrom, vTo, arcLength);
+
   for(i=0; i<MAX_V; i++)
   {
     input >> vFrom >> vTo;
     //intGraph.AddEdge(vFrom, vTo, arcLength);
-    //charGraph.AddEdge(v, /*toVertex,*/ w);
+    //intGraph.AddEdge(v, /*toVertex,*/ w);
   }
   cout << endl;
-  // cout << "Max Node Value is: " << intGraph.MaxNode() << endl;
+  //cout << "Max Node Value is: " << intGraph.MaxNode() << endl;
   cout << "Min Node Value is: "; // Node with smallest number of edges / connections
   //intGraph.MinNode();
   output.close();
